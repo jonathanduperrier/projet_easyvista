a8e82ce85436f6e83e2f9f8a8e0d0314c38e4e93
jonathanduperrier <jonathan.duperrier@gmail.com>
2020-11-04 22:44:33 +0100

    .

--- controller.js
+++ controller.js
@@ -5,6 +5,5 @@ angular.module('YTJSLibApp')
 .controller('videoController', function($scope, $stateParams, $sce){
     $scope.videoId = $stateParams.id;
     $scope.videoTitle = $stateParams.title;
-    //$scope.videoLink = 'https://www.youtube.com/embed/' + $scope.videoId;
     $scope.videoLink = $sce.trustAsResourceUrl("//www.youtube.com/embed/" + $scope.videoId);
 });
