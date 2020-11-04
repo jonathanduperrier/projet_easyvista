7493050db9a56cc29d15601c04828fbb7ce28144
jonathanduperrier <jonathan.duperrier@gmail.com>
2020-11-04 22:41:50 +0100

    .

--- controller.js
+++ controller.js
@@ -1,27 +1,3 @@
-// angular.module("YTJSLibApp")
-// // Le controller de la liste
-// .controller("DreamListController", ["$scope", "dreams", function($scope, dreams){
-//   // $scope.$parent === ui-view.$scope
-//   angular.extend($scope, {dreams});
-// }])
-// // Le controller du détail
-// .controller("DreamDetailController", ["$scope", "dream", "dreams", function($scope, dream, dreams){
-//   // $scope.$parent === "dreams".$scope
-//   // $scope.$parent.$parent === ui-view.$scope
-//   // On peut récupérer tous les éléments contenu dans le resolve du $state courant (dream) ainsi ceux des resolves des $states parents (dreams)
-//   angular.extend($scope, {dream});
-// }]);
-
-// var YTJSLibApp = angular.module('YTJSLibApp', []);
-
-// YTJSLibApp.controller('homeController', function($scope){
-//     $scope.toto = "toto";
-// });
-
-// YTJSLibApp.controller('videoUserController', function($scope){
-//     $scope.toto = "videoUserController";
-// });
-
 angular.module('YTJSLibApp')
 .controller('homeController', function($scope){
     $scope.toto = "toto";
